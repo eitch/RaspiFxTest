a333bfa79170b2a1709dba24429fe8730510fd22
Robert von Burg <eitch@eitchnet.ch>
2021-11-29 21:58:37 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+open module com.pi4j.mvc {
+     // Pi4J Modules
+    requires com.pi4j;
+    requires com.pi4j.library.pigpio;
+    requires com.pi4j.plugin.pigpio;
+    requires com.pi4j.plugin.raspberrypi;
+    requires com.pi4j.plugin.mock;
+    uses com.pi4j.extension.Extension;
+    uses com.pi4j.provider.Provider;
+
+    // for logging
+    requires org.slf4j;
+
+
+    // JavaFX
+    requires javafx.base;
+    requires javafx.controls;
+
+
+    // Module Exports
+
+
+}
